90fd7f59bddab89f0f32d06e02d57cd4f9d77519
NganHaVan <vannunganha@gmail.com>
2024-03-03 00:12:45 +0200

    Complete Hero section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: "btn_dark_green" | "btn_green" | "btn_white_text";
 };
 function Button({ title, type, variant, icon }: ButtonProps) {
   return (
