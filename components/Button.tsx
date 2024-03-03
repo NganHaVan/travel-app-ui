abefe23b2a85ee1045d7615c8a2cd8439149232c
NganHaVan <vannunganha@gmail.com>
2024-03-03 18:13:11 +0200

    complete GetApp UI

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,13 +4,21 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green" | "btn_green" | "btn_white_text";
+  variant:
+    | "btn_dark_green"
+    | "btn_green"
+    | "btn_white_text"
+    | "btn_white"
+    | "btn_dark_green_outline";
+  full?: boolean;
 };
-function Button({ title, type, variant, icon }: ButtonProps) {
+function Button({ title, type, variant, icon, full }: ButtonProps) {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border cursor-pointer ${variant} ${
+        full && "w-full"
+      }`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap">{title}</label>
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,5 +1,40 @@
 import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
 export default function GetApp() {
-  return <div>GetApp</div>;
+  return (
+    <section className="flexCenter w-full flex-col pb-100px">
+      <div className="get-app">
+        {/* LEFT */}
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12 text-white">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px] text-gray-400">
+            Get for free now!
+          </h2>
+          <p className="regular-16 text-gray-400">
+            Available on iOS and Android
+          </p>
+          <div className="flex w-full flex-col xl:flex-row gap-3 whitespace-nowrap">
+            <Button
+              title="Appstore"
+              type="button"
+              icon="/apple.svg"
+              variant="btn_white"
+              full
+            />
+            <Button
+              title="Playstore"
+              type="button"
+              icon="/android.svg"
+              variant="btn_dark_green_outline"
+              full
+            />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-end">
+          <Image src="/phones.png" width={550} height={870} alt="phones" />
+        </div>
+      </div>
+    </section>
+  );
 }
